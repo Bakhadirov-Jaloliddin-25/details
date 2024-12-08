8fa780451ff9616f4c5d500e044d377f481923e3
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2024-12-09 00:24:21 +0500

    first commit

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import { useStateValue } from "../../context";
+import { IoCartOutline, IoCartSharp } from "react-icons/io5";
 
 const Detail = () => {
-  const { setWishlist, wishlist } = useStateValue();
+  const { setWishlist, wishlist, setCart, cart } = useStateValue();
   const [data, setData] = useState(null);
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
@@ -35,6 +36,15 @@ const Detail = () => {
     } else {
       setWishlist((prev) => [...prev, product]);
     }
+  };
+
+  const handleAddToCart = (product) => {
+    const exists = cart.some((item) => item.id === product.id);
+    setCart((prev) =>
+      exists
+        ? prev.filter((item) => item.id !== product.id)
+        : [...prev, { ...product, amount: 1 }]
+    );
   };
 
   if (loading) {
@@ -84,7 +94,7 @@ const Detail = () => {
         <p className="text-gray-600 mb-6">{data?.description}</p>
         <button
           onClick={() => handleLike(data)}
-          className="flex items-center gap-2 px-6 py-3 bg-gradient-to-r from-green-300 to-emerald-400 text-white font-medium rounded-lg shadow-lg transition-all duration-300 hover:brightness-90"
+          className="flex items-center gap-2 px-6 my-5 py-3 bg-gradient-to-r from-green-300 to-emerald-400 text-white font-medium rounded-lg shadow-lg transition-all duration-300 hover:brightness-90"
           aria-label="Toggle Wishlist"
         >
           {wishlist?.some((item) => item.id === data?.id) ? (
@@ -96,6 +106,22 @@ const Detail = () => {
             {wishlist?.some((item) => item.id === data?.id)
               ? "Remove from Wishlist"
               : "Add to Wishlist"}
+          </span>
+        </button>
+        <button
+          onClick={() => handleAddToCart(data)}
+          className="flex items-center gap-2 px-6 my-5 py-3 bg-blue-500 text-white font-medium rounded-lg shadow-lg transition-all duration-300 hover:brightness-90"
+          aria-label="Toggle Cart"
+        >
+          {cart.some((item) => item.id === data?.id) ? (
+            <IoCartSharp className="w-[18px] h-[18px] text-amber-500" />
+          ) : (
+            <IoCartOutline className="w-[18px] h-[18px]" />
+          )}
+          <span className="text-sm font-medium">
+            {cart?.some((item) => item.id === data?.id)
+              ? "Already in the Cart"
+              : "Add to Cart"}
           </span>
         </button>
       </div>
